558dbd582f787afdccb24da0ef363e8fa65b2526
Alex Gabites <alex@netspeed.net.nz>
2020-03-04 16:04:00 +1300

    test commenting conditions

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,15 @@ const createMessage = () => undefined;
 
 async function run(): Promise<void> {
   try {
-    // Get environment variables
-    const netlifyAuthToken = process.env.NETLIFY_AUTH_TOKEN;
-    const siteId = process.env.NETLIFY_SITE_ID;
-
-    if (!netlifyAuthToken && !siteId) {
-      core.setFailed(`NETLIFY_AUTH_TOKEN and NETLIFY_SITE_ID are required env variables`);
-    }
-
-    // Get action inputs
+    // Get required action inputs
     // const githubToken = core.getInput('github-token', { required: true });
+    const netlifyAuthToken = core.getInput('netlify-auth-token', { required: true });
+    const siteId = core.getInput('netlify-site-id', { required: true });
     const buildDir = core.getInput('build-dir', { required: true });
+
+    // Get optional action inputs
+    const commentOnCommit = core.getInput('comment-on-commit') === 'true';
+    const commentOnPullRequest = core.getInput('comment-on-pull-request') === 'true';
     const functionsDir = core.getInput('functions-dir') || null;
     const configPath = core.getInput('config-path') || null;
     const draft = core.getInput('draft') === 'true';
@@ -42,7 +40,18 @@ async function run(): Promise<void> {
       maxRetry,
     });
 
-    process.stdout.write(JSON.stringify(github.context, null, 2));
+    const isCommit = Object.keys(github.context.payload).includes('commits');
+    const isPullRequest = Object.keys(github.context.payload).includes('pull_request');
+
+    process.stdout.write(JSON.stringify({ isCommit, isPullRequest }, null, 2));
+
+    if (isCommit && commentOnCommit) {
+      process.stdout.write(`\ncomment on commit: ${github.context.sha}`);
+    }
+
+    if (isPullRequest && commentOnPullRequest) {
+      process.stdout.write(`\ncomment on pull request: ${github.context.payload.pull_request?.number}`);
+    }
 
     // Comment with deploy URL on PR
     // const githubClient = new github.GitHub(githubToken);
