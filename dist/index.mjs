c0898f20c0f0c129abdbb832141e8ac2cca2de7c
Hebilicious <xsh4k3@gmail.com>
2022-04-04 16:28:53 +0200

    feat: Build with tsup and bump node version

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,24 +1,8 @@
-var __create = Object.create;
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target, mod));
-
 // src/index.ts
-var core = __toESM(require("@actions/core"));
-var github = __toESM(require("@actions/github"));
-var exec = __toESM(require("@actions/exec"));
-var path = __toESM(require("path"));
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as exec from "@actions/exec";
+import * as path from "path";
 
 // src/util.ts
 var getDeployUrl = (isDraft, deploy) => isDraft ? deploy.deploy_ssl_url : deploy.ssl_url;
